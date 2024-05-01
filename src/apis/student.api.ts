73be6bf9a015ce23829b5b198b461a7df9ad6a5f
NguyenDucHuy <duchuytv2102@gmail.com>
2024-05-01 14:51:17 +0700

    working with AddStudent.tsx, understand how to use useMutation

---
+++ src/apis/student.api.ts
@@ -0,0 +1,15 @@
+import { Student, studentList } from 'types/student.type'
+import http from 'utils/http'
+
+export const getStudents = (page: number | string, limit: number | string) => {
+  return http.get<studentList>('students', {
+    params: {
+      _page: page,
+      _limit: limit
+    }
+  })
+}
+
+export const addStudent = (student: Omit<Student, 'id'>) => {
+  return http.post<Student>('students', student)
+}
